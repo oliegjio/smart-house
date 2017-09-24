749e006d57a23cb12dbfeec0e9bcc715664d5d5d
oliegjio <oliegjio@gmail.com>
2017-09-24 18:11:03 +0300

    Commit

--- index.js
+++ index.js
@@ -37,19 +37,18 @@ sPort.on('data', (data) => {
     var shortDate = hours + ':' + minutes + ':' + seconds 
     var longDate = year + '-' + month + '-' + day + ' ' + shortDate
 
-    // Fuck
     si.cpuTemperature((data) => {
         temperature = data.max
 
         dataNow = `${shortDate} ${newData}${temperature}\n`
         console.log(dataNow)
 
-        var fs = require('fs');
-        fs.appendFile("/tmp/test", dataNow, function(error) {
-            if(error) {
-                return console.log(error);
-            }
-        }); 
+        // var fs = require('fs');
+        // fs.appendFile("/tmp/test", dataNow, function(error) {
+        //     if(error) {
+        //         return console.log(error);
+        //     }
+        // }); 
 
         // db.exec(`INSERT INTO temperatures (date, t1, t2, t3, t4, t5, t6) VALUES("${shortDate}", "${splitData[0]}", "${splitData[1]}", "${splitData[2]}", "${splitData[3]}", "${splitData[4]}", "${temperature}")`)
     }) 
@@ -70,36 +69,11 @@ var sendDataNow = (response) => {
     response.write(dataNow)
 }
 
-// var sendDataAll = (response) => {
-//     db.all(`SELECT * FROM temperatures`, (error, rows, response) => {
-//         var newData = ''
-//         var date, t1, t2, t3, t4, t5, t6
-//         for (var i = 0; rows.length; i++) {
-//             if (!rows[i]) continue
-//             date = rows[i].date
-//             t1 = rows[i].t1
-//             t2 = rows[i].t2
-//             t3 = rows[i].t3
-//             t4 = rows[i].t4
-//             t5 = rows[i].t5
-//             t6 = rows[i].t6
-//             newData += `${date} ${t1} ${t2} ${t3} ${t4} ${t5} ${t6} \n`
-//             // console.log(newData)
-//         }
-//         // console.log(newData)
-//         response.write(newData)
-//         console.log('asdfasdfa')
-//     })
-// }
-
 const requestHandler = (request, response) => {
     switch (request.url) {
         case '/get-data-now':
             sendDataNow(response)
             break
-        // case '/get-data-all':
-        //     sendDataAll(response)
-        //     break
         default:
             response.write('Wrong Request!')
     }
